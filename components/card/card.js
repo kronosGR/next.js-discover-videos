66be8908ea3e53fc563a2cbb2682f7f05596f553
KronosGR <geo.elgeo@gmail.com>
2022-02-01 07:24:50 +0100

    image error handling

--- components/card/card.js
+++ components/card/card.js
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 import styles from './card.module.css';
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
+  const { imgUrl="/static/cliffor.webp", size = 'medium' } = props;
+
+  const [imgSrc, setImgSrc] = useState(imgUrl)
 
   const classMap = {
     large: styles.lgItem,
@@ -11,11 +14,23 @@ const Card = (props) => {
     small: styles.smItem,
   };
 
+  const handleOnError=()=>{
+    setImgSrc('/static/cliffor.webp')
+  }
+
   return (
     <div className={styles.container}>
       Card
       <div className={classMap[size]}>
-        <Image src={imgUrl} alt='image' width={300} height={300} layout='fill' className={styles.cardImg} />
+        <Image
+          src={imgSrc}
+          alt='image'
+          width={300}
+          height={300}
+          layout='fill'
+          onError={handleOnError}
+          className={styles.cardImg}
+        />
       </div>
     </div>
   );
